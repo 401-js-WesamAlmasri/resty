17c4c409d215890c6719b68ca288173fe14e7f38
Wesam Al-Masri <mr0virus@gmail.com>
2021-06-21 19:44:50 +0300

    live api request with handling errors

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import Footer from './components/Footer/Footer';
 import './App.scss';
+import Results from './components/Results/Results';
 
-function App() {
-  return (
-    <div className='app'>
-      <Header />
-      <Form />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: null,
+      headers: null,
+      results: null,
+      error: null,
+      loading: false,
+    };
+
+    this.updateResultsHandler = this.updateResultsHandler.bind(this);
+  }
+
+  updateResultsHandler(count, headers, results, error, loading) {
+    this.setState({ count, headers, results, error, loading });
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <Header />
+        <Form updateResultsHandler={this.updateResultsHandler} />
+        <div className='results_body'>
+          <Results
+            count={this.state.count}
+            headers={this.state.headers}
+            results={this.state.results}
+            error={this.state.error}
+            loading={this.state.loading}
+          />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,9 +5,9 @@ class Form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: 'http://localhost:3000/',
+      url: 'https://swapi.dev/api/people/',
       method: 'get',
-      results: {},
+      body: {}
     };
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
@@ -19,14 +19,36 @@ class Form extends React.Component {
     });
   }
 
-  onSubmitHandler(e){
+  async onSubmitHandler(e) {
     e.preventDefault();
-    this.setState({
-      results: {
-        url: e.target.url.value,
-        method: e.target.method.value,
+    this.props.updateResultsHandler(null, null, null, null, true);
+
+    try {
+      const requestOptions = {
+        method: this.state.method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: this.state.method !== 'get' ? JSON.parse(`${this.state.body}`) : null,
+      };
+
+      const response = await fetch(this.state.url, requestOptions);
+      const jsonResponse = await response.json();
+
+      if(response.status > 399){
+        this.props.updateResultsHandler(null, null, null, response.statusText, false);
+        return;
       }
-    })
+      this.props.updateResultsHandler(
+        jsonResponse.count,
+        response.headers.get('content-type'),
+        jsonResponse.results,
+        null,
+        false
+      );
+    } catch (e) {
+      this.props.updateResultsHandler(null, null, null, e.message, false);
+    }
   }
 
   render() {
@@ -46,38 +68,62 @@ class Form extends React.Component {
             <button type='submit'>GO!</button>
           </div>
           <div onChange={this.onChangeHandler} className='methods_container'>
-            <label htmlFor='get-method' className={`${this.state.method === 'get' ? 'active' : ''}`}>
-              GET
-              <input
-                defaultChecked
-                type='radio'
-                value='get'
-                name='method'
-                id='get-method'
-              />
-            </label>
-            <label htmlFor='post-method' className={`${this.state.method === 'post' ? 'active': ''}`}>
-              POST
-              <input type='radio' value='post' name='method' id='post-method' />
-            </label>
-            <label htmlFor='put-method' className={`${this.state.method === 'put' ? 'active' : ''}`}>
-              PUT
-              <input type='radio' value='put' name='method' id='put-method' />
-            </label>
-            <label htmlFor='delete-method' className={`${this.state.method === 'delete' ? 'active' : ''}`}>
-              DELETE
-              <input
-                type='radio'
-                value='delete'
-                name='method'
-                id='delete-method'
-              />
-            </label>
+            <div>
+              <label
+                htmlFor='get-method'
+                className={`${this.state.method === 'get' ? 'active' : ''}`}
+              >
+                GET
+                <input
+                  defaultChecked
+                  type='radio'
+                  value='get'
+                  name='method'
+                  id='get-method'
+                />
+              </label>
+              <label
+                htmlFor='post-method'
+                className={`${this.state.method === 'post' ? 'active' : ''}`}
+              >
+                POST
+                <input
+                  type='radio'
+                  value='post'
+                  name='method'
+                  id='post-method'
+                />
+              </label>
+              <label
+                htmlFor='put-method'
+                className={`${this.state.method === 'put' ? 'active' : ''}`}
+              >
+                PUT
+                <input type='radio' value='put' name='method' id='put-method' />
+              </label>
+              <label
+                htmlFor='delete-method'
+                className={`${this.state.method === 'delete' ? 'active' : ''}`}
+              >
+                DELETE
+                <input
+                  type='radio'
+                  value='delete'
+                  name='method'
+                  id='delete-method'
+                />
+              </label>
+            </div>
+            <textarea
+              className='req-body'
+              placeholder='Request body'
+              name='body'
+              id='body'
+              cols='30'
+              rows='10'
+            ></textarea>
           </div>
         </form>
-        <div className='results_container'>
-          <p>{this.state.results.method} {this.state.results.url}</p>
-        </div>
       </>
     );
   }
